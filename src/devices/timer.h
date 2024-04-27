1912b2f67e1529792a285b1088778d5f6fa3c8ae
waleedemadyaya <waleedemad797@gmail.com>
2024-04-27 17:47:15 +0300

    type changes

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -40,6 +40,6 @@ void timer_ndelay (int64_t nanoseconds);
 
 void timer_print_stats (void);
 
-static bool cmp_fnc(const struct list_elem *a, const struct list_elem *b, void * aux UNUSED);
+bool cmp_fnc(const struct list_elem *a, const struct list_elem *b, void * aux UNUSED);
 
 #endif /* devices/timer.h */
